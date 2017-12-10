f4f1d79cdd7a4d9e455e53614e82cbbfdd0d8efe
ldebrot <ldebrot@yahoo.de>
2017-12-10 22:35:13 +0100

    added decrease for touch on orderrelative

--- src/app/components/beneficiaire/quizz/cs-orderrelative/cs-orderrelative.component.ts
+++ src/app/components/beneficiaire/quizz/cs-orderrelative/cs-orderrelative.component.ts
@@ -1,7 +1,6 @@
 import { ReadwritebufferService } from './../../../../services/readwritebuffer.service';
 import { QuizzService } from 'app/services/quizz.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
     selector: 'app-cs-orderrelative',
     templateUrl: './cs-orderrelative.component.html',
@@ -20,7 +19,7 @@ export class CsOrderrelativeComponent implements OnInit {
         this.populatebuttonitems();
         this.setupbuttonservice();
     }
-
+    
     public buttonitems : any[] = [];//holds button options, but stored according to position (not id!)
     public button_value : any = {};//holds values of each button, uses option id (not position!)
     public button_class : any = {};//holds classes of each button, uses option id (not position!)
@@ -32,12 +31,6 @@ export class CsOrderrelativeComponent implements OnInit {
     public minpoints : number = 0;
     public treasurelevels : number = 4;//this is linked to the css classes, where there are 5 levels
     public treasureid = 99; //all 99 values pertain to treasure
-    
-
-    public tryme() : boolean {
-        console.log("duh");
-        return false;
-    }
 
     public increase(optionid){
         if (this.button_value[this.treasureid] > 0 && this.button_value[optionid] < this.maxpoints) {
@@ -57,7 +50,7 @@ export class CsOrderrelativeComponent implements OnInit {
         }
         return false;
     }
-
+    
     public populatebuttonitems () {
         this.buttonitems = [];
         for (let i = 0; i < this.quizzservice.currentcardobject.parameters.options.length; i++) {
@@ -65,25 +58,25 @@ export class CsOrderrelativeComponent implements OnInit {
             this.buttonitems.push(this.quizzservice.currentcardobject["option"+String(temp_optionid)]);
         }
     }
-
+    
     public getmeclasssuffix(optionid){
         let temp_divider = this.maxpoints / this.treasurelevels;
         let temp_value = this.button_value[optionid];
         let temp_level = String(Math.round(temp_value/temp_divider));
         return temp_level;
     }
-
+    
     public updaterecipientclasses() {
         for (let i = 0; i < this.quizzservice.currentcardobject.parameters.options.length; i++) {
             let temp_optionid = this.quizzservice.currentcardobject.parameters.options[i];
             this.pointrecipient_class[temp_optionid] = "csorderrelative_" + this.getmeclasssuffix(temp_optionid) + " " + this.pointrecipientclass;
         }
     }
-
+    
     public updatetreasureclass() {
         this.pointrecipient_class[this.treasureid] = "csorderrelative_" + this.getmeclasssuffix(this.treasureid) + " " + this.treasureclassbasic;  
     }
-
+    
     public setupbuttonservice () {
         for (let i = 0; i < this.quizzservice.currentcardobject.parameters.options.length; i++) {
             let temp_optionid = this.quizzservice.currentcardobject.parameters.options[i];
@@ -92,12 +85,12 @@ export class CsOrderrelativeComponent implements OnInit {
         }
         this.updatetreasureclass();
         this.updaterecipientclasses();
-
+        
         console.log("this.button_class");
         console.log(this.button_class);
         console.log("this.pointrecipient_class");
         console.log(this.pointrecipient_class);
     }
     
-
+    
 }
