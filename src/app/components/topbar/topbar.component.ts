af7d089d78ceb593bc03bfa91e8c5a06084b6fdd
ldebrot <ldebrot@yahoo.de>
2018-02-11 15:13:14 +0100

    set up topbar and menu, rearranged cardset component nicely

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {SidebarModule} from 'primeng/sidebar';
 
 @Component({
-  selector: 'app-topbar',
-  templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+    selector: 'app-topbar',
+    templateUrl: './topbar.component.html',
+    styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent implements OnInit {
+    
+    public display : boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor() { }
+    
+    ngOnInit() {
+    }
+    
 }
